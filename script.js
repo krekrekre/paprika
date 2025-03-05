3ea8531da7f422db9d44d8821ee8dc661656a0e9
Nemanja Kosanovic <nemanjakosanovic7@gmail.com>
2025-03-05 21:59:00 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+
+const swiper = new Swiper(".mySwiper", {
+        spaceBetween: 30,
+        loop: true,
+        centeredSlides: true,
+        autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+        },
+        pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        }
+        });
+
+const toggleButton = document.querySelector(".menu-toggle");
+const menu = document.querySelector(".menu");
+const closeMenu = document.querySelector(".menu-close");
+
+toggleButton.addEventListener("click", () => {
+        menu.classList.toggle("active"); // Show or hide the menu
+        });
+
+
+closeMenu.addEventListener("click", () => {
+        menu.classList.toggle("active"); // Show or hide the menu
+        });
+
+
+
+AOS.init();

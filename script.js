5a2e6b6e00589c7cd40606b0ddcb0ff3b9d432d8
Nemanja Kosanovic <nemanjakosanovic7@gmail.com>
2025-03-06 15:00:45 +0100

    formated all files

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
-
 const swiper = new Swiper(".mySwiper", {
-        spaceBetween: 30,
-        loop: true,
-        centeredSlides: true,
-        autoplay: {
-        delay: 4000,
-        disableOnInteraction: false,
-        },
-        pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        }
-        });
+  spaceBetween: 30,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
 
 const toggleButton = document.querySelector(".menu-toggle");
 const menu = document.querySelector(".menu");
 const closeMenu = document.querySelector(".menu-close");
 
 toggleButton.addEventListener("click", () => {
-        menu.classList.toggle("active"); // Show or hide the menu
-        });
-
+  menu.classList.toggle("active"); // Show or hide the menu
+});
 
 closeMenu.addEventListener("click", () => {
-        menu.classList.toggle("active"); // Show or hide the menu
-        });
-
+  menu.classList.toggle("active"); // Show or hide the menu
+});
 
 AOS.init();
